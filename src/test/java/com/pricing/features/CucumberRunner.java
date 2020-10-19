573574c7bd64b23f71f81c2fbc1ec54603baf50d
Tony <tpg@blueyonder.co.uk>
2020-10-20 00:10:48 +0100

    Minor update to cucumber runner

--- src/test/java/com/pricing/features/CucumberRunner.java
+++ src/test/java/com/pricing/features/CucumberRunner.java
@@ -1,8 +1,10 @@
 package com.pricing.features;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber.json"})
 public class CucumberRunner {
 }
