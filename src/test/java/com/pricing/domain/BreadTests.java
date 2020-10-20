b49f57b8c915eb4e9bbe6cb9a443e2ecc791d4ec
Tony <tpg@blueyonder.co.uk>
2020-10-20 09:17:38 +0100

    Added bread and soup as a stock item unit tests

---
+++ src/test/java/com/pricing/domain/BreadTests.java
@@ -0,0 +1,18 @@
+package com.pricing.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static com.pricing.domain.Bread.bread;
+import static com.pricing.domain.ProductUnit.Loaf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BreadTests implements InPence {
+    @Test
+    public void givenBreadIsAProduct_whenMadeAStockItem_thenItIsAStockItem() {
+        StockItem actual = bread();
+
+        assertThat(actual.getProduct().getName()).isEqualTo("Bread");
+        assertThat(actual.getProduct().getProductUnit()).isEqualTo(Loaf);
+        assertThat(actual.getProduct().getCost()).isEqualTo(pence(80));
+    }
+}
---
+++ src/test/java/com/pricing/domain/SoupTests.java
@@ -0,0 +1,19 @@
+package com.pricing.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static com.pricing.domain.ProductUnit.Bottle;
+import static com.pricing.domain.ProductUnit.Tin;
+import static com.pricing.domain.Soup.soup;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SoupTests implements InPence {
+    @Test
+    public void givenSoupIsAProduct_whenMadeAStockItem_thenItIsAStockItem() {
+        StockItem actual = soup();
+
+        assertThat(actual.getProduct().getName()).isEqualTo("Soup");
+        assertThat(actual.getProduct().getProductUnit()).isEqualTo(Tin);
+        assertThat(actual.getProduct().getCost()).isEqualTo(pence(65));
+    }
+}
